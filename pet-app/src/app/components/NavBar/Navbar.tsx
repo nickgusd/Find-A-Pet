6149039a41f5514bddce7ade24ac77331d28d7ac
Nick Guimarais <Nick.guimarais@gmail.com>
2023-01-28 13:24:25 -0800

    add nav updates

--- pet-app/src/app/components/NavBar/Navbar.tsx
+++ pet-app/src/app/components/NavBar/Navbar.tsx
@@ -1,5 +1,33 @@
+import { Link } from "react-router-dom";
+
+import logo from "../../assets/Find-a-Pet-logo.svg";
+
 import styles from "./styles.module.css";
 
 export const Navbar = () => {
-  return <div className={styles.container}>Navbar</div>;
+  const leftNavItems = ["Breeds", "Organizations"];
+  const rightNavItems = ["Sign Up", "Log In"];
+  return (
+    <div className={styles.container}>
+      <div className={styles.leftWrapper}>
+        <div>
+          <img src={logo} alt="logo" />
+        </div>
+        <div className={styles.leftNavItems}>
+          {leftNavItems.map((item, idx) => (
+            <Link key={idx} to={`/${item.toLocaleLowerCase()}`}>
+              {item}
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className={styles.rightWrapper}>
+        {rightNavItems.map((item, idx) => (
+          <Link key={idx} to={`/${item.toLocaleLowerCase()}`}>
+            {item}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
