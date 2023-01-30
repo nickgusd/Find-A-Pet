3add8c3a2af46e9b77b6a9f6716ee872551bac13
Nick Guimarais <Nick.guimarais@gmail.com>
2023-01-29 21:29:25 -0800

    updates to home page

--- pet-app/src/app/pages/Home.tsx
+++ pet-app/src/app/pages/Home.tsx
@@ -1,16 +1,29 @@
+import { useEffect } from "react";
 import Search from "../components/Search/Search";
+import { getAnimals, selectAnimals } from "../../app/slice/animalsSlice";
+import { useAppSelector, useAppDispatch } from "../../app/hooks";
 
 import hero from "../assets/dog-cat-hero.png";
-
 import styles from "./Home.module.css";
 
 export const Home = () => {
+  const animals = useAppSelector(selectAnimals);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getAnimals("https://api.petfinder.com/v2/animals/"));
+  }, []);
+  console.log("animals", animals);
   return (
     <div className={styles.container}>
       <div className={styles.heroWrapper}>
         <img src={hero} alt="hero" />
         <div className={styles.searchWrapper}>
           <Search />
+        </div>
+        <div className={styles.header}>
+          <h1>Find your new pet</h1>
+          <p>There are thousands of animals waiting for a new home</p>
         </div>
       </div>
     </div>
