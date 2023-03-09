4c5e100c4843add20e4488b94091a92b2c0c25d4
Nick Guimarais <Nick.guimarais@gmail.com>
2023-03-08 20:35:30 -0800

    make mobile homepage and add mobile responsiveness

--- src/app/components/NavBar/Navbar.tsx
+++ src/app/components/NavBar/Navbar.tsx
@@ -7,6 +7,7 @@ import {
   useNavigate,
 } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useMediaQuery } from "react-responsive";
 
 import logo from "../../assets/high-res-logo.svg";
 import { BsFillHeartFill } from "react-icons/bs";
@@ -14,6 +15,7 @@ import { getSearchParams } from "../../utils/search";
 import { getPath } from "../../utils/string";
 import Search from "../Search/Search";
 import { List } from "../List/List";
+import { FiMenu } from "react-icons/fi";
 
 import styles from "./styles.module.css";
 
@@ -24,6 +26,7 @@ export const Navbar = () => {
   const [locationValue, setLocationValue] = useState("");
   const [noLocation, setNoLocation] = useState(false);
   const [mouse, setMouse] = useState(false);
+  const isMobile = useMediaQuery({ maxWidth: 768 });
 
   const params = {
     type: searchValue,
@@ -108,65 +111,140 @@ export const Navbar = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.leftWrapper}>
-        <Link to="/">
-          <img src={logo} alt="logo" />
-        </Link>
-        <div className={styles.leftNavItems}>
-          <Link to={`/${getPath("Organizations")}`}>Organizations</Link>
-          <div className={styles.animals} onMouseEnter={handleMouseOver}>
-            Animals
-            {mouse && <List onMouseLeave={handleMouseLeave} />}
+    <div
+      className={`${styles.container} ${
+        isMobile ? styles.containerMobile : ""
+      }`}
+    >
+      {!isMobile && (
+        <>
+          <div className={styles.leftWrapper}>
+            <Link to="/">
+              <img src={logo} alt="logo" />
+            </Link>
+            <div className={styles.leftNavItems}>
+              <Link to={`/${getPath("Organizations")}`}>Organizations</Link>
+              <div className={styles.animals} onMouseEnter={handleMouseOver}>
+                Animals
+                {mouse && <List onMouseLeave={handleMouseLeave} />}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      {location.pathname === "/search" && (
-        <div onKeyDown={onKeyEnter}>
-          <Search
-            isSearchPage
-            onClick={onSearch}
-            size="small"
-            icon="search"
-            onChangeLocation={handleChangeLocation}
-            onChangeSearch={handleChangeSearch}
-            noLocation={noLocation}
-          />
-        </div>
-      )}
-      <div className={styles.rightWrapper}>
-        <BsFillHeartFill
-          onClick={
-            !isAuthenticated
-              ? () => {
+          {location.pathname === "/search" && (
+            <div onKeyDown={onKeyEnter}>
+              <Search
+                isMobile={false}
+                isSearchPage
+                onClick={onSearch}
+                size="small"
+                icon="search"
+                onChangeLocation={handleChangeLocation}
+                onChangeSearch={handleChangeSearch}
+                noLocation={noLocation}
+              />
+            </div>
+          )}
+          <div className={styles.rightWrapper}>
+            <BsFillHeartFill
+              onClick={
+                !isAuthenticated
+                  ? () => {
+                      loginWithRedirect({
+                        appState: {
+                          returnTo:
+                            window.location.pathname + window.location.search,
+                        },
+                      });
+                    }
+                  : () => navigate("/favorites")
+              }
+            />
+            <span />
+            {!isAuthenticated && (
+              <div
+                onClick={() =>
                   loginWithRedirect({
                     appState: {
                       returnTo:
                         window.location.pathname + window.location.search,
                     },
-                  });
+                  })
                 }
-              : () => navigate("/favorites")
-          }
-        />
-        <span />
-        {!isAuthenticated && (
-          <div
-            onClick={() =>
-              loginWithRedirect({
-                appState: {
-                  returnTo: window.location.pathname + window.location.search,
-                },
-              })
-            }
-          >
-            Log In
+              >
+                Log In
+              </div>
+            )}
+            {isAuthenticated && (
+              <div onClick={() => logoutWithRedirect()}>Log Out</div>
+            )}
           </div>
-        )}
-        {isAuthenticated && (
-          <div onClick={() => logoutWithRedirect()}>Log Out</div>
-        )}
-      </div>
+        </>
+      )}
+      {isMobile && (
+        <>
+          <div className={styles.leftWrapperMobile}>
+            <Link to="/">
+              <img src={logo} alt="logo" />
+            </Link>
+            {/* <div className={styles.leftNavItems}>
+              <Link to={`/${getPath("Organizations")}`}>Organizations</Link>
+              <div className={styles.animals} onMouseEnter={handleMouseOver}>
+                Animals
+                {mouse && <List onMouseLeave={handleMouseLeave} />}
+              </div>
+            </div> */}
+          </div>
+          {/* {location.pathname === "/search" && (
+            <div onKeyDown={onKeyEnter}>
+              <Search
+                isMobile={false}
+                isSearchPage
+                onClick={onSearch}
+                size="small"
+                icon="search"
+                onChangeLocation={handleChangeLocation}
+                onChangeSearch={handleChangeSearch}
+                noLocation={noLocation}
+              />
+            </div>
+          )} */}
+          <div className={styles.rightWrapperMobile}>
+            <FiMenu color="#525252" />
+            {/* <BsFillHeartFill
+              onClick={
+                !isAuthenticated
+                  ? () => {
+                      loginWithRedirect({
+                        appState: {
+                          returnTo:
+                            window.location.pathname + window.location.search,
+                        },
+                      });
+                    }
+                  : () => navigate("/favorites")
+              }
+            /> */}
+            {/* <span />
+            {!isAuthenticated && (
+              <div
+                onClick={() =>
+                  loginWithRedirect({
+                    appState: {
+                      returnTo:
+                        window.location.pathname + window.location.search,
+                    },
+                  })
+                }
+              >
+                Log In
+              </div>
+            )}
+            {isAuthenticated && (
+              <div onClick={() => logoutWithRedirect()}>Log Out</div>
+            )} */}
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router";
+import { useMediaQuery } from "react-responsive";
 import { createSearchParams } from "react-router-dom";
 import Search from "../components/Search/Search";
 import { getSearchParams } from "../utils/search";
 
 import hero from "../assets/dog-cat-hero.png";
+import mobileHero from "../assets/Dogs-Cats-mobile.png";
 import styles from "./Home.module.css";
 
 export const Home = () => {
@@ -12,6 +14,8 @@ export const Home = () => {
   const [searchValue, setSearchValue] = useState("");
   const [locationValue, setLocationValue] = useState("");
   const [noLocation, setNoLocation] = useState(false);
+  const isTabletOrMobile = useMediaQuery({ maxWidth: 1224 });
+  const isSmallMobile = useMediaQuery({ maxWidth: 768 });
   const params = {
     type: searchValue,
     location: locationValue,
@@ -47,24 +51,60 @@ export const Home = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.heroWrapper}>
-        <img src={hero} alt="hero" />
-        <div className={styles.searchWrapper} onKeyDown={onSearch}>
-          <Search
-            isSearchPage={false}
-            onClick={null}
-            size="large"
-            icon="search"
-            noLocation={noLocation}
-            onChangeSearch={handleChangeSearch}
-            onChangeLocation={handleChangeLocation}
+      {!isTabletOrMobile && (
+        <div className={styles.heroWrapper}>
+          <img
+            src={!isTabletOrMobile ? hero : mobileHero}
+            alt="hero"
+            className={isTabletOrMobile ? styles.heroMobile : ""}
           />
+          <div className={styles.searchWrapper} onKeyDown={onSearch}>
+            <Search
+              isMobile={false}
+              isSearchPage={false}
+              onClick={null}
+              size="large"
+              icon="search"
+              noLocation={noLocation}
+              onChangeSearch={handleChangeSearch}
+              onChangeLocation={handleChangeLocation}
+            />
+          </div>
+          <div className={styles.header}>
+            <h1 className={isTabletOrMobile ? styles.mobileHeader : ""}>
+              Find your new pet
+            </h1>
+            <p className={isTabletOrMobile ? styles.mobileText : ""}>
+              There are thousands of animals waiting for a new home
+            </p>
+          </div>
         </div>
-        <div className={styles.header}>
-          <h1>Find your new pet</h1>
-          <p>There are thousands of animals waiting for a new home</p>
+      )}
+      {isTabletOrMobile && (
+        <div className={styles.mobileHeroWrapper}>
+          <div onKeyDown={onSearch}>
+            <Search
+              isMobile
+              isSearchPage={isSmallMobile}
+              onClick={null}
+              size="large"
+              icon="search"
+              noLocation={noLocation}
+              onChangeSearch={handleChangeSearch}
+              onChangeLocation={handleChangeLocation}
+            />
+          </div>
+          <div className={styles.mobileHeader}>
+            <h1>Find your new pet</h1>
+            <p className={styles.mobileText}>
+              There are thousands of animals waiting for a new home
+            </p>
+          </div>
+          <div className={styles.mobileImageWrapper}>
+            <img src={mobileHero} alt="hero" className={styles.heroMobile} />
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
